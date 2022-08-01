0e9f0be1fe676120a37c05901cf34c30865e0c07
Adrien Dion <adion81@gmail.com>
2022-08-01 12:30:21 -0500

    08012022-nav-adrien removed border and ring from page title

--- src/components/Nav/navStatic.js
+++ src/components/Nav/navStatic.js
@@ -4,11 +4,8 @@ const css = {
                 bg-slate-100
                 dark:bg-slate-900 
                 dark:text-white`,
-    h1: ` p-1 px-2 
-        rounded border-2 border-orange-500 ring-slate-500 ring-2
-        text-4xl text-center lg:text-2xl 
-        text-slate-900 dark:text-slate-50 
-        dark:ring-orange-500 dark:border-slate-500`,
+    h1: `text-5xl text-center lg:text-3xl 
+        text-slate-900 dark:text-slate-50`,
     menuIcon: `text-5xl`,
     spanK: `text-orange-500`,
     switch: `dark:bg-slate-900 text-5xl`
